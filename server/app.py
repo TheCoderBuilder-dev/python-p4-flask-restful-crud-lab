831492ce09e40d1859bdacd34d1a21212d083e28
Brian Munene <brianinesh@gmail.com>
2025-07-03 07:46:10 +0300

    crud

--- server/app.py
+++ server/app.py
@@ -1,55 +1,51 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
 from models import db, Plant
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
 
-api = Api(app)
+@app.route('/plants/<int:id>', methods=['GET', 'PATCH'])
+def handle_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
 
+    if request.method == 'GET':
+        return jsonify({
+            "id": plant.id,
+            "name": plant.name,
+            "image": plant.image,
+            "price": plant.price,
+            "is_in_stock": plant.is_in_stock
+        }), 200
 
-class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
+    elif request.method == 'PATCH':
         data = request.get_json()
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+            db.session.commit()
+        return jsonify({
+            "id": plant.id,
+            "name": plant.name,
+            "image": plant.image,
+            "price": plant.price,
+            "is_in_stock": plant.is_in_stock
+        }), 200
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
 
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    db.session.delete(plant)
+    db.session.commit()
+    return '', 204
--- server/seed.py
+++ server/seed.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python3
-
-from app import app
 from models import db, Plant
-
+from app import app
 
 with app.app_context():
-
+    db.create_all()
     Plant.query.delete()
-
-    aloe = Plant(
-        id=1,
-        name="Aloe",
-        image="./images/aloe.jpg",
-        price=11.50,
-        is_in_stock=True,
-    )
-
-    zz_plant = Plant(
-        id=2,
-        name="ZZ Plant",
-        image="./images/zz-plant.jpg",
-        price=25.98,
-        is_in_stock=False,
-    )
-
-    db.session.add_all([aloe, zz_plant])
     db.session.commit()
+
+    seed_data = [
+        {"name": "Aloe", "image": "./images/aloe.jpg", "price": 11.50},
+        {"name": "Eucalyptus", "image": "./images/eucalyptus.jpg", "price": 9.99},
+        {"name": "Monstera", "image": "./images/monstera.jpg", "price": 19.99, "is_in_stock": False}
+    ]
+
+    for p in seed_data:
+        db.session.add(Plant(**p))
+    db.session.commit()
+    print(" Seeded DB")
